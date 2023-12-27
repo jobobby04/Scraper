10a5501956b374ebe5abc7519b639ecace53ccb9
Jobobby04 <jobobby04@users.noreply.github.com>
2023-12-27 13:10:39 -0500

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "1.9.21"
 }
 
 group = "com.jobobby"
@@ -12,10 +12,10 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("com.squareup.okhttp3:okhttp:4.10.0")
-    implementation("org.jsoup:jsoup:1.14.3")
-    implementation("com.squareup:kotlinpoet:1.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("org.jsoup:jsoup:1.17.1")
+    implementation("com.squareup:kotlinpoet:1.15.3")
 }
 
 java {
